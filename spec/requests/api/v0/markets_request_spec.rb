b288ec1b34ccc4a7150a6f5e9c516ffdeac0b3cd
Nick Sacco <nicksacco10@gmail.com>
2023-08-28 12:04:42 -0700

    refactor tests for json api serializer gem

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -8,8 +8,35 @@ RSpec.describe "Market API", type: :request do
       get "/api/v0/markets"
 
       expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-      markets = JSON.parse(response.body)
+      markets = JSON.parse(response.body, symbolize_names: true)
+
+      markets[:data].each do |market|
+        expect(market).to have_key(:id)
+        expect(market[:id]).to be_an(String)
+        expect(market).to have_key(:type)
+        expect(market[:type]).to eq('market')
+        expect(market).to have_key(:attributes)
+
+        expect(market[:attributes]).to be_a(Hash)
+        expect(market[:attributes]).to have_key(:name)
+        expect(market[:attributes][:name]).to be_a(String)
+        expect(market[:attributes]).to have_key(:street)
+        expect(market[:attributes][:street]).to be_a(String)
+        expect(market[:attributes]).to have_key(:city)
+        expect(market[:attributes][:city]).to be_a(String)
+        expect(market[:attributes]).to have_key(:county)
+        expect(market[:attributes][:county]).to be_a(String)
+        expect(market[:attributes]).to have_key(:state)
+        expect(market[:attributes][:state]).to be_a(String)
+        expect(market[:attributes]).to have_key(:zip)
+        expect(market[:attributes][:zip]).to be_a(String)
+        expect(market[:attributes]).to have_key(:lat)
+        expect(market[:attributes][:lat]).to be_a(String)
+        expect(market[:attributes]).to have_key(:lon)
+        expect(market[:attributes][:lon]).to be_a(String)
+      end
     end
   end
 end
