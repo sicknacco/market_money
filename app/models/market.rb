af89d17f4e559a14ab452c759606b6c09feced46
Nick Sacco <nicksacco10@gmail.com>
2023-08-28 11:21:31 -0700

    feat: models defined with relationships and validations

---
+++ app/models/market.rb
@@ -0,0 +1,14 @@
+class Market < ApplicationRecord
+  has_many :market_vendors
+  has_many :vendors, through: :market_vendors
+
+  validates :name,
+            :street,
+            :city,
+            :county,
+            :state,
+            :zip,
+            :lat,
+            :lon,
+            presence: true
+end
