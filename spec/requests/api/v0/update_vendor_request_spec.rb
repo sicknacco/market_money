1ae6af7db1b34587966498bd38a39939db435235
Nick Sacco <nicksacco10@gmail.com>
2023-08-29 10:53:56 -0700

    test: sad path for missing attribute on update

--- spec/requests/api/v0/update_vendor_request_spec.rb
+++ spec/requests/api/v0/update_vendor_request_spec.rb
@@ -44,5 +44,31 @@ RSpec.describe "Updating a Vendor", type: :request do
       expect{Vendor.find(000000)}.to raise_error(ActiveRecord::RecordNotFound)
       expect(error_details[:errors][0][:detail]).to eq("Couldn't find Vendor with 'id'=000000")
     end
+
+    it 'sad path - missing required attributes' do
+      vendor = create(:vendor)
+      vendor_params = {
+        contact_name: "",
+        credit_accepted: false
+      }
+      headers = {
+        CONTENT_TYPE: "application/json",
+        ACCEPT: "application/json"
+      }
+
+      patch "/api/v0/vendors/#{vendor.id}", headers: headers, params: JSON.generate(vendor_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      error_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_data).to be_a Hash
+      expect(error_data).to have_key(:errors)
+      expect(error_data[:errors]).to be_an Array
+      expect(error_data[:errors][0]).to be_a Hash
+      expect(error_data[:errors][0]).to have_key(:detail)
+      expect(error_data[:errors][0][:detail]).to eq("Validation failed: Value is missing or empty")
+    end
   end
 end
