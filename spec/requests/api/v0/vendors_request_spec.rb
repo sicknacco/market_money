a4f29801f38dfa60e664da87dc626efd0283df3f
Nick Sacco <nicksacco10@gmail.com>
2023-08-28 19:51:40 -0700

    test: complete tests for finding a single vendor and attributes

--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -61,4 +61,43 @@ RSpec.describe "Vendor API", type: :request do
       expect(market[:errors][0][:detail]).to eq("Couldn't find Market with 'id'=0")
     end
   end
+
+  describe 'can return a single vendor and its attributes' do
+    it 'happy path' do
+      market = create(:market)
+      vendor = create(:vendor)
+      MarketVendor.create!(market_id: market.id, vendor_id: vendor.id)
+
+      headers = {
+        "CONTENT_TYPE" => "application/json",
+        "ACCEPT" => "application/json"
+      }
+
+      get "/api/v0/vendors/#{vendor.id}", headers: headers
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)    
+      
+      vendor_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(vendor_data[:data]).to be_a Hash
+      expect(vendor_data[:data]).to have_key(:id)
+      expect(vendor_data[:data][:id]).to eq(vendor.id.to_s)
+      expect(vendor_data[:data]).to have_key(:type)
+      expect(vendor_data[:data][:type]).to eq('vendor')
+      expect(vendor_data[:data]).to have_key(:attributes)
+
+      expect(vendor_data[:data][:attributes]).to be_a Hash
+      expect(vendor_data[:data][:attributes]).to have_key(:name)
+      expect(vendor_data[:data][:attributes][:name]).to eq(vendor.name)
+      expect(vendor_data[:data][:attributes]).to have_key(:description)
+      expect(vendor_data[:data][:attributes][:description]).to eq(vendor.description)
+      expect(vendor_data[:data][:attributes]).to have_key(:contact_name)
+      expect(vendor_data[:data][:attributes][:contact_name]).to eq(vendor.contact_name)
+      expect(vendor_data[:data][:attributes]).to have_key(:contact_phone)
+      expect(vendor_data[:data][:attributes][:contact_phone]).to eq(vendor.contact_phone)
+      expect(vendor_data[:data][:attributes]).to have_key(:credit_accepted)
+      expect(vendor_data[:data][:attributes][:credit_accepted]).to eq(vendor.credit_accepted)
+    end
+  end
 end
