7ee2e7aed6b79ce6a1c8feb1b46ffd55b5856afb
Nick Sacco <nicksacco10@gmail.com>
2023-08-29 13:11:44 -0700

    feat: implement sad path and add validations to model

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,10 +1,12 @@
 class Api::V0::MarketVendorsController < ApplicationController
   def create
-    market = Market.find(params[:market_id])
-    vendor = Vendor.find(params[:vendor_id])
+    market = Market.find_by(id: params[:market_id])
+    vendor = Vendor.find_by(id: params[:vendor_id])
     mv = MarketVendor.new(market: market, vendor: vendor)
     if mv.save
       render json: { "message": "Successfully added #{vendor.name} to #{market.name}" }, status: 201
+    else market.nil? || vendor.nil?
+      render json: { "errors": [{ "detail": "Validation failed: market or vendor does not exist" }] }, status: 404
     end
   end
 end
