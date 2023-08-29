fe0b5e631a7b83460eb9093283fec77a8c0781a4
Nick Sacco <nicksacco10@gmail.com>
2023-08-29 08:51:07 -0700

    test: sad path tests for creating a vendor with missing attributes

--- spec/requests/api/v0/post_vendor_request_spec.rb
+++ spec/requests/api/v0/post_vendor_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Creating a new vendor", type: :request do
   describe 'POST /api/v0/vendors' do
-    it 'can create a new vendor in the body of the request' do
+    it 'happy path - can create a new vendor in the body of the request' do
       vendor_params = {
         name: "Super Cool Vendor",
         description: "We sell super cool things",
@@ -44,6 +44,32 @@ RSpec.describe "Creating a new vendor", type: :request do
       expect(vendor[:data][:attributes][:contact_phone]).to eq(new_vendor.contact_phone)
       expect(vendor[:data][:attributes]).to have_key(:credit_accepted)
       expect(vendor[:data][:attributes][:credit_accepted]).to eq(new_vendor.credit_accepted)
+    end
+
+    it 'sad path - missing required attributes' do
+      vendor_params = {
+        name: "Buzzy Bees",
+        description: "local honey and wax products",
+        credit_accepted: true
+      }
+      headers = {
+        CONTENT_TYPE: "application/json",
+        ACCEPT: "application/json"
+      }
+
+      post '/api/v0/vendors', headers: headers, params: JSON.generate(vendor_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      error_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_data).to be_a Hash
+      expect(error_data).to have_key(:errors)
+      expect(error_data[:errors]).to be_an Array
+      expect(error_data[:errors][0]).to be_a Hash
+      expect(error_data[:errors][0]).to have_key(:detail)
+      expect(error_data[:errors][0][:detail]).to eq("Validation failed: Contact name can't be blank, Contact phone can't be blank")
     end
   end
 end
