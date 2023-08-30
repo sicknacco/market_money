6557734fe61b6885c9fe906b1934f95dcf39f61e
Nick Sacco <nicksacco10@gmail.com>
2023-08-30 15:46:42 -0700

    test: atm request tests, all atm facade tests, atm poro tests started but need fixed

--- spec/facades/atm_facade_spec.rb
+++ spec/facades/atm_facade_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AtmFacade do
-  describe 'ATM locations', :vcr do
-  #   it 'initailize', :vcr do
-  #     market = create(:market)
-  #     atm_facade = AtmFacade.new(market)
-
-  #     expect(atm_facade).to be_a AtmFacade
-  #   end
-
-    it 'can retrieve atms close to given coordinates' do
+  describe 'ATM locations' do
+    it 'returns nearest atms', :vcr do
       market = create(:market)
-      atm_facade = AtmFacade.new(market)
 
-      expect(atm_facade.atms_near_market).to be_a Array
-      expect(atm_facade.atms_near_market.first).to be_a Atm
+      atms = AtmFacade.get_atms(market)
+
+      expect(atms).to be_an(Array)
+      atms.each do |atm|
+        expect(atm).to be_a(Atm)
+        expect(atm.name).to be_a(String)
+        expect(atm.address).to be_a(String)
+        expect(atm.lat).to be_a(Float)
+        expect(atm.lon).to be_a(Float)
+        expect(atm.distance).to be_a(Float)
+      end
     end
   end
 end
---
+++ spec/requests/api/v0/atms/nearby_atms_request_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Atms API', type: :request do
+  describe 'GET /api/v0/markets/:id/nearest_atms' do
+    it 'returns nearest atms' do
+      market = create(:market)
+
+      headers = {
+        CONTENT_TYPE: "application/json",
+        ACCEPT: "application/json"
+      }
+
+      get "/api/v0/markets/#{market.id}/nearest_atms", headers: headers
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      atms = JSON.parse(response.body, symbolize_names: true)
+
+      expect(atms[:data]).to be_an(Array)
+      atms[:data].each do |atm|
+        expect(atm).to have_key(:id)
+        expect(atm[:id]).to eq(nil)
+        expect(atm).to have_key(:type)
+        expect(atm[:type]).to eq('atm')
+        expect(atm).to have_key(:attributes)
+        expect(atm[:attributes]).to be_a(Hash)
+
+        expect(atm[:attributes]).to have_key(:name)
+        expect(atm[:attributes][:name]).to eq('ATM')
+        expect(atm[:attributes]).to have_key(:address)
+        expect(atm[:attributes][:address]).to be_a(String)
+        expect(atm[:attributes]).to have_key(:lat)
+        expect(atm[:attributes][:lat]).to be_a(Float)
+        expect(atm[:attributes]).to have_key(:lon)
+        expect(atm[:attributes][:lon]).to be_a(Float)
+        expect(atm[:attributes]).to have_key(:distance)
+        expect(atm[:attributes][:distance]).to be_a(Float)
+      end
+    end
+  end
+end
