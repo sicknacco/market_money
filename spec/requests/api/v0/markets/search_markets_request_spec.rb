4b2f27a0bbe12d24e6865fffa54b363520d9c479
Nick Sacco <nicksacco10@gmail.com>
2023-08-30 10:56:00 -0700

    test: complete happy path tests for searching

--- spec/requests/api/v0/markets/search_markets_request_spec.rb
+++ spec/requests/api/v0/markets/search_markets_request_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Search Markets', type: :request do
+  before(:each) do
+    @market1 = create(:market, name:'Cool Place', city: 'Grand Junction', state: 'Colorado')
+    market2 = create(:market, name:'Okay Place', city: 'Grand Junction', state: 'Colorado')
+    market3 = create(:market, name:'Lame Place', city: 'San Diegoo', state: 'California')
+  end
   describe 'GET /api/v0/markets/search' do
     it 'can search for markets by state, city, and name' do
-      market1 = create(:market, name:'Cool Place', city: 'Grand Junction', state: 'Colorado')
-      market2 = create(:market, name:'Okay Place', city: 'Grand Junction', state: 'Colorado')
-      market3 = create(:market, name:'Lame Place', city: 'San Diegoo', state: 'California')
-
       headers = {
         CONTENT_TYPE: "application/json",
         ACCEPT: "application/json"
@@ -28,20 +29,98 @@ RSpec.describe 'Search Markets', type: :request do
       expect(markets[:data]).to be_an Array
 
       markets[:data].each do |market|
-        expect(market[:id]).to eq(market1.id.to_s)
+        expect(market[:id]).to eq(@market1.id.to_s)
         expect(market[:type]).to eq('market')
         expect(market[:attributes]).to be_a Hash
 
-        expect(market[:attributes][:name]).to eq(market1.name)
-        expect(market[:attributes][:street]).to eq(market1.street)
-        expect(market[:attributes][:city]).to eq(market1.city)
-        expect(market[:attributes][:county]).to eq(market1.county)
-        expect(market[:attributes][:state]).to eq(market1.state)
-        expect(market[:attributes][:zip]).to eq(market1.zip)
-        expect(market[:attributes][:lat]).to eq(market1.lat)
-        expect(market[:attributes][:lon]).to eq(market1.lon)
-        expect(market[:attributes][:vendor_count]).to eq(market1.vendor_count)
+        expect(market[:attributes][:name]).to eq(@market1.name)
+        expect(market[:attributes][:street]).to eq(@market1.street)
+        expect(market[:attributes][:city]).to eq(@market1.city)
+        expect(market[:attributes][:county]).to eq(@market1.county)
+        expect(market[:attributes][:state]).to eq(@market1.state)
+        expect(market[:attributes][:zip]).to eq(@market1.zip)
+        expect(market[:attributes][:lat]).to eq(@market1.lat)
+        expect(market[:attributes][:lon]).to eq(@market1.lon)
+        expect(market[:attributes][:vendor_count]).to eq(@market1.vendor_count)
       end
+    end
+
+    it 'can search for markets by state and city' do
+      headers = {
+        CONTENT_TYPE: "application/json",
+        ACCEPT: "application/json"
+      }
+      q_params = {
+        state: 'Colorado',
+        city: 'Grand Junction'
+      }
+
+      get '/api/v0/markets/search', headers: headers, params: q_params
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      markets = JSON.parse(response.body, symbolize_names: true)
+      expect(markets[:data]).to be_an Array
+      expect(markets[:data].count).to eq(2)
+    end
+
+    it 'can search for markets by state and name' do
+      headers = {
+        CONTENT_TYPE: "application/json",
+        ACCEPT: "application/json"
+      }
+      q_params = {
+        state: 'Colorado',
+        name: 'Cool Place'
+      }
+
+      get '/api/v0/markets/search', headers: headers, params: q_params
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      markets = JSON.parse(response.body, symbolize_names: true)
+      expect(markets[:data]).to be_an Array
+      expect(markets[:data].count).to eq(1)
+    end
+
+    it 'can search for markets by state' do
+      headers = {
+        CONTENT_TYPE: "application/json",
+        ACCEPT: "application/json"
+      }
+      q_params = {
+        state: 'Colorado'
+      }
+
+      get '/api/v0/markets/search', headers: headers, params: q_params
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      markets = JSON.parse(response.body, symbolize_names: true)
+      expect(markets[:data]).to be_an Array
+      expect(markets[:data].count).to eq(2)
+    end
+
+    it 'can search for markets by name' do
+      headers = {
+        CONTENT_TYPE: "application/json",
+        ACCEPT: "application/json"
+      }
+      q_params = {
+        name: 'Lame Place'
+      }
+
+      get '/api/v0/markets/search', headers: headers, params: q_params
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      markets = JSON.parse(response.body, symbolize_names: true)
+      expect(markets[:data]).to be_an Array
+      expect(markets[:data].count).to eq(1)
     end
   end
 end
